26411f790c3a5c937e1141065b1e3fd4550e973b
ricky <caopeng8787@163.com>
2018-06-23 15:46:06 +0800

    添加缩进

--- basic/note.py
+++ basic/note.py
@@ -14,3 +14,28 @@ print("welcome to python3 world") # 第二个注释
 """
 
 print("welcome")
+
+
+# 行与缩进
+# python最具特色的就是使用缩进来表示代码块，不需要使用大括号 {} 。
+# 缩进的空格数是可变的，但是同一个代码块的语句必须包含相同的缩进空格数
+if True:
+	print("True")
+else:
+	print("False")
+
+if True:
+	print("Answer")
+	print("True")
+else:
+	print("Answer")
+  #print("False") # unindent does not match any outer indentation level 未缩进与任何外部缩进级别不匹配
+
+# Python 通常是一行写完一条语句，但如果语句很长，我们可以使用反斜杠(\)来实现多行语句
+total = item_one + \
+        item_two + \
+        item_three
+
+# 在 [], {}, 或 () 中的多行语句，不需要使用反斜杠(\)
+total = ['item_one', 'item_two', 'item_three',
+        'item_four', 'item_five']
