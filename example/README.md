062f4c5ae974c9df21908472e73cbc92fba13924
ricky <caopeng8787@163.com>
2018-06-25 10:08:25 +0800

    添加平方根

--- example/README.md
+++ example/README.md
@@ -1,3 +1,4 @@
 实例
 sum.py 求和
 sqrt.py 平方根
+equation.py 二次方程
--- example/sqrt.py
+++ example/sqrt.py
@@ -2,4 +2,8 @@
 
 num = float(input('请输入一个数字: '))
 num_sqrt = num ** 0.5
-#print(' %0.3f 的平方根为 %0.3f' % (num ,num_sqrt))
+print(' %0.3f 的平方根为 %0.3f' % (num , num_sqrt))
+
+import cmath
+sqrt = cmath.sqrt(num)
+print(' {0} 的平方根为 {1}'.format(num , sqrt)) # 4 2+0j
